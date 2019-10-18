e0be3951c4596381249cbd3a23b7f05cf79a0133
VadimIvanoff <42637856+VadimIvanoff@users.noreply.github.com>
2019-10-18 10:34:09 +0300

    Base API added

--- src/controllers/home/home.controller.ts
+++ src/controllers/home/home.controller.ts
@@ -1,9 +1,36 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Head, Header, Logger, Param, Post, Put, Query } from '@nestjs/common';
 import { ConfigService } from '../../services/config/config.service';
+import { User } from '../../models/User';
 
 @Controller('home')
 export class HomeController {
   constructor(private readonly configSrv: ConfigService) {
+  }
+
+  @Post('add')
+  create(@Body() user: User) {
+    return 'This action adds a new user';
+  }
+
+  @Get()
+  @Header('Cache-Control', 'none')
+  findAll(@Query() query: any) {
+    return `This action returns all users ${query.limit}`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} user`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUser: User) {
+    return `This action updates a #${id} user`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} user`;
   }
 
   @Get('test')
